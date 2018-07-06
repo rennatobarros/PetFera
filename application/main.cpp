d5a949aeaa4559c7d8f134878c8cf413133173ca
Renato Barros <rennatobarros@gmail.com>
2018-07-06 01:26:14 -0300

    Definições do funcionario adicionado

--- application/main.cpp
+++ application/main.cpp
@@ -1,3 +1,15 @@
+/** 
+ * @mainpage PetFera
+ * @author Renato Barros de Lima Freitas e Antonio Marcos de Oliveira
+ * @date 07/07/2018
+ * @version 1.0
+ *
+ * @file application/main.cpp
+ * @brief Arquivo principal do programa
+*/
+
+#include <iostream>
+
 int main(int argc, char const *argv[])
 {
 	

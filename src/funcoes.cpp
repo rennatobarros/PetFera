8ae458bbfd40e63026628fdc7b074d44a8346795
Antonio <marcos_antonio97@gmail.com>
2018-07-07 09:58:21 -0300

    tratamento na abertura de arquivos

--- src/funcoes.cpp
+++ src/funcoes.cpp
@@ -105,78 +105,74 @@ void initAnimais(ifstream arq, map<int, Anfibio> &anfibios, map<int, Reptil> &re
 
 					mamiferos->emplace(stoi(id), animal);
 				}
+				if (classe == "Amphibia"){
+					Anfibio animal;
+
+					animal.setId(stoi(id));
+					animal.setClasse(classe);
+					animal.setNome(nome);
+					animal.setCientifico(cientifico);
+					animal.setSexo(sexo[0]);
+					animal.setTamanho(stof(tamanho));
+					animal.setDieta(dieta);
+					animal.setVeterinario(stoi(vet));
+					animal.setTratador(stoi(tratador));
+					animal.setBatismo(batismo);
+
+					anfibios.emplace(stoi(id), animal);
+				}
+				else if(classe == "Reptilia"){
+					Reptil animal;
+
+					animal.setId(stoi(id));
+					animal.setClasse(classe);
+					animal.setNome(nome);
+					animal.setCientifico(cientifico);
+					animal.setSexo(sexo[0]);
+					animal.setTamanho(stof(tamanho));
+					animal.setDieta(dieta);
+					animal.setVeterinario(stoi(vet));
+					animal.setTratador(stoi(tratador));
+					animal.setBatismo(batismo);
+
+					repteis.emplace(stoi(id), animal);
+				}else if(classe == "Aves"){
+					Ave animal;
+
+					animal.setId(stoi(id));
+					animal.setClasse(classe);
+					animal.setNome(nome);
+					animal.setCientifico(cientifico);
+					animal.setSexo(sexo[0]);
+					animal.setTamanho(stof(tamanho));
+					animal.setDieta(dieta);
+					animal.setVeterinario(stoi(vet));
+					animal.setTratador(stoi(tratador));
+					animal.setBatismo(batismo);
+
+					aves.emplace(stoi(id), animal);
+				}else if(classe == "Mammalia"){
+					Mamifero animal;
+
+					animal.setId(stoi(id));
+					animal.setClasse(classe);
+					animal.setNome(nome);
+					animal.setCientifico(cientifico);
+					animal.setSexo(sexo[0]);
+					animal.setTamanho(stof(tamanho));
+					animal.setDieta(dieta);
+					animal.setVeterinario(stoi(vet));
+					animal.setTratador(stoi(tratador));
+					animal.setBatismo(batismo);
+
+					mamiferos.emplace(stoi(id), animal);
+				}
 			}
 		}
 	}catch(FalhaNoArquivo &f){
 		cerr << f.what() << endl;
 	}catch(...){
 		cerr << "Erro desconhecido." << endl;
-
-		if (classe == "Amphibia"){
-			Anfibio animal;
-
-			animal.setId(stoi(id));
-			animal.setClasse(classe);
-			animal.setNome(nome);
-			animal.setCientifico(cientifico);
-			animal.setSexo(sexo[0]);
-			animal.setTamanho(stof(tamanho));
-			animal.setDieta(dieta);
-			animal.setVeterinario(stoi(vet));
-			animal.setTratador(stoi(tratador));
-			animal.setBatismo(batismo);
-
-			anfibios.emplace(stoi(id), animal);
-			
-		}else if(classe == "Reptilia"){
-			Reptil animal;
-
-			animal.setId(stoi(id));
-			animal.setClasse(classe);
-			animal.setNome(nome);
-			animal.setCientifico(cientifico);
-			animal.setSexo(sexo[0]);
-			animal.setTamanho(stof(tamanho));
-			animal.setDieta(dieta);
-			animal.setVeterinario(stoi(vet));
-			animal.setTratador(stoi(tratador));
-			animal.setBatismo(batismo);
-
-			repteis.emplace(stoi(id), animal);
-
-		}else if(classe == "Aves"){
-			Ave animal;
-
-			animal.setId(stoi(id));
-			animal.setClasse(classe);
-			animal.setNome(nome);
-			animal.setCientifico(cientifico);
-			animal.setSexo(sexo[0]);
-			animal.setTamanho(stof(tamanho));
-			animal.setDieta(dieta);
-			animal.setVeterinario(stoi(vet));
-			animal.setTratador(stoi(tratador));
-			animal.setBatismo(batismo);
-
-			aves.emplace(stoi(id), animal);
-		
-		}else if(classe == "Mammalia"){
-			Mamifero animal;
-
-			animal.setId(stoi(id));
-			animal.setClasse(classe);
-			animal.setNome(nome);
-			animal.setCientifico(cientifico);
-			animal.setSexo(sexo[0]);
-			animal.setTamanho(stof(tamanho));
-			animal.setDieta(dieta);
-			animal.setVeterinario(stoi(vet));
-			animal.setTratador(stoi(tratador));
-			animal.setBatismo(batismo);
-
-			mamiferos.emplace(stoi(id), animal);
-		}
-
 	}
 }
 
@@ -185,63 +181,64 @@ void initFuncionarios(ifstream arq, map<int, Tratador> &tratadores, map<int, Vet
 
 	try{
 		arq.open("files/funcionarios.csv");
+		if(!arq.good()) FalhaNoArquivo();
+		else{
+			/** String para armazenar cada linha do arquivo. */
+			string ler_linha;
+
+			while(getline(arq, ler_linha)){
+				string id;
+				string funcao;
+				string nome;
+				string cpf;
+				string idade;
+				string tipo;
+				string fator;
+				string especialidade;
+				
+				stringstream ss(ler_linha);
+
+				getline(ss, id, ';');
+				getline(ss, funcao, ';');
+				getline(ss, nome, ';');
+				getline(ss, cpf, ';');
+				getline(ss, idade, ';');
+				getline(ss, tipo, ';');
+				getline(ss, fator, ';');
+				getline(ss, especialidade, ';');		
+
+				if (funcao == "Tratador"){
+					Tratador funcionario;
+
+					funcionario.setId(stoi(id));
+					funcionario.setFuncao(funcao);
+					funcionario.setNome(nome);
+					funcionario.setCpf(cpf);
+					funcionario.setIdade(stoi(idade));
+					funcionario.setTipoSanguineo(tipo);
+					funcionario.setFatorRH(fator[0]);
+					funcionario.setEspecialidade(especialidade);
+
+					tratadores.emplace(stoi(id), funcionario);
+			
+				}else if(funcao == "Veterinario"){
+					Veterinario funcionario;
+
+					funcionario.setId(stoi(id));
+					funcionario.setFuncao(funcao);
+					funcionario.setNome(nome);
+					funcionario.setCpf(cpf);
+					funcionario.setIdade(stoi(idade));
+					funcionario.setTipoSanguineo(tipo);
+					funcionario.setFatorRH(fator[0]);
+					funcionario.setEspecialidade(especialidade);
+
+					veterinarios.emplace(stoi(id), funcionario);
+				}
+			}
+		}
 	}catch(FalhaNoArquivo &f){
 		cerr << f.what() << endl;
-	}
-
-	/** String para armazenar cada linha do arquivo. */
-	string ler_linha;
-
-	while(getline(arq, ler_linha)){
-		string id;
-		string funcao;
-		string nome;
-		string cpf;
-		string idade;
-		string tipo;
-		string fator;
-		string especialidade;
-		
-		stringstream ss(ler_linha);
-
-		getline(ss, id, ';');
-		getline(ss, funcao, ';');
-		getline(ss, nome, ';');
-		getline(ss, cpf, ';');
-		getline(ss, idade, ';');
-		getline(ss, tipo, ';');
-		getline(ss, fator, ';');
-		getline(ss, especialidade, ';');		
-
-		if (funcao == "Tratador"){
-			Tratador funcionario;
-
-			funcionario.setId(stoi(id));
-			funcionario.setFuncao(funcao);
-			funcionario.setNome(nome);
-			funcionario.setCpf(cpf);
-			funcionario.setIdade(stoi(idade));
-			funcionario.setTipoSanguineo(tipo);
-			funcionario.setFatorRH(fator[0]);
-			funcionario.setEspecialidade(especialidade);
-
-			tratadores.emplace(stoi(id), funcionario);
-			
-		}else if(funcao == "Veterinario"){
-			Veterinario funcionario;
-
-			funcionario.setId(stoi(id));
-			funcionario.setFuncao(funcao);
-			funcionario.setNome(nome);
-			funcionario.setCpf(cpf);
-			funcionario.setIdade(stoi(idade));
-			funcionario.setTipoSanguineo(tipo);
-			funcionario.setFatorRH(fator[0]);
-			funcionario.setEspecialidade(especialidade);
-
-			veterinarios.emplace(stoi(id), funcionario);
-
-		}
-
-	}
+	}catch(...)
+		cerr << "Erro desconhecido." << endl;	
 }
